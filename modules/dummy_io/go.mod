477d1e4cef588e8e1ea7a80cd77a0f96a31af344
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-09-18 15:57:45 +0200

    add dummy_io

---
+++ modules/dummy_io/go.mod
@@ -0,0 +1,3 @@
+module github.com/Tomasz-Smelcerz-SAP/quality/modules/dummy_io
+
+go 1.22
---
+++ modules/dummy_io/io.go
@@ -0,0 +1,31 @@
+package dummy_io
+
+import (
+	"io"
+)
+
+type GetWriterFn func() io.WriteCloser
+
+// StringIterator is an interface for reading strings.
+type StringIterator interface {
+	Next() (string, error)
+	HasNext() bool
+}
+
+type StringReader struct {
+	index int
+	str   []string
+}
+
+func (r *StringReader) Next() (string, error) {
+	if r.index >= len(r.str) {
+		return "", io.EOF
+	}
+	s := r.str[r.index]
+	r.index++
+	return s, nil
+}
+
+func (r *StringReader) HasNext() bool {
+	return r.index < len(r.str)
+}
